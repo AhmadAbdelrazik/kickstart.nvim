565cc7562d945be456d4656f7a55d4f54aeb154e
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2024-08-24 18:35:40 +0300

    added treesitter context, undotree, lua line

--- lua/Abdelrazik/remaps.lua
+++ lua/Abdelrazik/remaps.lua
@@ -11,9 +11,11 @@ vim.keymap.set('x', '<leader>p', '"_dP')
 vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
 
 -- Map keys to insert new lines below or above
-
 vim.keymap.set('n', '<leader>o', ':normal o<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>O', ':normal O<CR>', { noremap = true, silent = true })
 
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Use jq program to format json files
+vim.keymap.set('n', '<leader>JQ', ':%!jq<CR>')
--- lua/custom/plugins/undotree.lua
+++ lua/custom/plugins/undotree.lua
@@ -1,4 +1,7 @@
 return {
   'mbbill/undotree',
-  vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle),
+  vim.keymap.set('n', '<leader>u', function()
+    vim.cmd.UndotreeToggle()
+    vim.cmd.UndotreeFocus()
+  end, { desc = '[U]ndotree' }),
 }
